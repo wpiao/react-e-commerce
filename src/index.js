5ea602b69e74b9832dcdd296195fac17dd3f9ba1
Wenhao Piao <wenhaopiao520@gmail.com>
2020-06-01 23:48:30 -0700

    Initial Env Setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app.js';
+
+const Main = props => {
+  <React.Fragment>
+    <App />
+  </React.Fragment>
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
